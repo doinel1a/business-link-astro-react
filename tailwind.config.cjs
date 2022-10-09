77efc1f6ebd00df606c66db94dc42ee981868f26
doinel1a <doinel1atanasiu@gmail.com>
2022-10-09 11:41:34 +0200

    Global colors css variables configured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,16 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-light': 'var(--primary-light)',
+				'primary-dark': 'var(--primary-dark)',
+				'color-light': 'var(--color-light)',
+				'color-dark': 'var(--color-dark)',
+				'accent-primary': 'var(--accent-primary)',
+				'accent-primary-state': 'var(--accent-primary-state)',
+			},
+		},
 	},
 	plugins: [],
 };
